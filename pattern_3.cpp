dbaf174a7b9bbe48adb94072d3b70cab95ebae81
Anmol Mahajan <a.mahajan110104@gmail.com>
2021-11-14 12:48:34 +0530

    initial commit

---
+++ pattern_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num=0;
+    int i=0,j=0,k=0;
+    cin>>num;
+    for(i=0;i<num;i++){
+        for(j=k;j<=num;j++){
+            cout<<" ";
+        }
+        for(j=0;j<=k;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        k++;
+    }
+    return 0;
+}
